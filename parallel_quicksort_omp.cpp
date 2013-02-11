c733cd37457500369fc09bf34c468178fb4ee042
Christian Magnerfelt <christian.magnerfelt@gmail.com>
2013-02-11 13:36:02 +0100

    Added parallel quicksort function

--- parallel_quicksort_omp.cpp
+++ parallel_quicksort_omp.cpp
@@ -6,13 +6,68 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <iterator>
 
 //! global variables
+const long g_sortCutoff = 1000;
+const std::size_t g_numOfPivots = 10;
 const std::size_t g_maxSize = 10000000;	//!< Maximum array size
 const std::size_t g_maxThreads = 128;		//!< Maximum number of threads
 std::size_t g_numThreads;
 std::size_t g_size;
 
+template <typename T, typename RandomIt>
+void parallelQuicksort(RandomIt start, RandomIt end)
+{
+	static std::vector<T> pivots (g_numOfPivots);
+	auto distance = std::distance(start, end);
+	if(distance < g_sortCutoff)
+	{
+		std::sort(start, end);
+	}
+	else
+	{
+		RandomIt it;
+		for(std::size_t i = 0; i < pivots.size(); ++i)
+		{
+			it = start;
+			std::advance(it, rand() % distance);
+			pivots[i] = *it;
+		}
+		// Sort pivots in acending order
+		std::sort(pivots.begin(), pivots.end());
+		
+		// Select the pivot value from the middle of the pivot vector
+		T pivotValue = pivots[pivots.size()/2];
+		
+		// Partition values so those less than pivot value are placed in front of the array
+		RandomIt middle = std::partition(start, end, [&pivotValue](const T & value){
+			if(value < pivotValue)
+				return true;
+			else
+				return false;
+		});
+		
+		// Sort as normal if middle is equal to start or end. 
+		// All elements are either the same or we selected a max or min value as pivot
+		if(middle == start || middle == end)
+		{
+			std::sort(start, end);
+			return;
+		}
+		
+		#pragma omp task shared(start, middle)
+		{
+			parallelQuicksort<T, RandomIt>(start, middle);
+		}
+		#pragma omp task shared(middle, end)
+		{
+			parallelQuicksort<T, RandomIt>(middle, end);
+		}
+		
+		#pragma omp taskwait
+	}
+}
 int main(int argc, const char *argv [])
 {
 	double start_time, end_time;
@@ -39,7 +94,7 @@ int main(int argc, const char *argv [])
 	{
 		#pragma omp single nowait
 		{
-			std::sort(arrayData.begin(), arrayData.end());
+			parallelQuicksort<int, std::vector<int>::iterator>(arrayData.begin(), arrayData.end());
 		}
 	}
 	
